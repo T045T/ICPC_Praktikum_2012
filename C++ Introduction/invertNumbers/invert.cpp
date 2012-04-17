e45fe901a7d086f0044376dd10298754ee093687
Nils Berg <berg.nils@gmail.com>
2012-04-17 17:49:57 +0200

    Technically unsupported behavior ftw...

--- C++ Introduction/invertNumbers/invert.cpp
+++ C++ Introduction/invertNumbers/invert.cpp
@@ -8,9 +8,8 @@ int array[max_n];
 int main () {
 	int n = 0;
 	while(cin >> array[n++]);
-	n--;
-	
-	cout << "Es gibt " << n-- << " Zahlen" << endl;
+		
+	cout << "Es gibt " << (--n)-- << " Zahlen" << endl;
 	while(n >= 0) cout << array[n--] << " ";
 	cout << endl;
 }
