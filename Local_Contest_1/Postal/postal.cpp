cfeb45c1b74c18fbc0feebfb4bd7475bdbbc919c
Nils Berg <berg.nils@gmail.com>
2012-05-22 18:38:28 +0200

    MOAR LOOPS

--- Local_Contest_1/Postal/postal.cpp
+++ Local_Contest_1/Postal/postal.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
@@ -10,22 +11,29 @@ int main() {
 	double sum;
 	while (cin >> houses) {
 		sum = 0.0;
-		vector<pair<double, double> > houseVec;
+		vector<vector<vector<pair<double, double> > > > map = 
+			vector<vector<vector<pair<double, double> > > >(10, vector<vector<pair<double, double> > >(10));
+		//vector<pair<double, double> > houseVec;
 		for (int i = 0; i < houses; i++) {
 			pair<double, double> input;
 			cin >> input.first;
 			cin >> input.second;
-			houseVec.push_back(input);
+			map[(int) input.first][(int) input.second].push_back(input);
+			//houseVec.push_back(input);
 		}
-		sort (houseVec.begin(), houseVec.begin() + houseVec.size());
+		//sort (houseVec.begin(), houseVec.begin() + houseVec.size());
 		count = 0;
-		for (int i = 0; i < houseVec.size(); i++) {
-			for (int j = i+1; j < houseVec.size(); j++) {
-				if (   (int) houseVec[i].first  < (int) houseVec[j].first
-					&& (int) houseVec[i].second < (int) houseVec[j].second) {
-					sum += houseVec[j].first - houseVec[i].first;
-					sum += houseVec[j].second - houseVec[i].second;
-					count++;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < map[i][j].size(); k++) {
+					for(int inner_i = i; inner_i < 10; inner_i++) {
+						for (int inner_j = j; inner_j < 10; inner_j++) {
+							for (int inner_k = 0; inner_k < map[inner_i][inner_j].size(); inner_k++) {
+								sum += map[inner_i][inner_j][inner_k].first - map[i][j][k].first;
+								sum += map[inner_i][inner_j][inner_k].second - map[i][j][k].second;
+							}
+						}
+					}
 				}
 			}
 		}
