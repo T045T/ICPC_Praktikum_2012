df618a26bc4da0a2d19f9a5635b85d93719e72cb
Nils Berg <berg.nils@gmail.com>
2012-05-22 19:34:31 +0200

    Y U NOOOO? :(

--- Local_Contest_1/Postal/postal.cpp
+++ Local_Contest_1/Postal/postal.cpp
@@ -6,37 +6,46 @@
 
 using namespace std;
 
+struct mapSquare {
+	double value;
+	int houses;
+};
+
 int main() {
 	int houses, count;
 	double sum;
+	double tmp, tmp_i, tmp_j;
 	while (cin >> houses) {
 		sum = 0.0;
-		vector<vector<vector<pair<double, double> > > > map = 
-			vector<vector<vector<pair<double, double> > > >(10, vector<vector<pair<double, double> > >(10));
-		//vector<pair<double, double> > houseVec;
+		mapSquare input;
+		input.value = 0.0;
+		input.houses = 0;
+		vector<pair<double, double> > houseVec;
+		vector<vector<mapSquare > > map = vector<vector<mapSquare> > (10, vector<mapSquare> (10, input));
 		for (int i = 0; i < houses; i++) {
+			cin >> tmp_i >> tmp_j;
 			pair<double, double> input;
-			cin >> input.first;
-			cin >> input.second;
-			map[(int) input.first][(int) input.second].push_back(input);
-			//houseVec.push_back(input);
+			input.first = tmp_i;
+			input.second = tmp_j;
+			houseVec.push_back(input);
+			map[(int) tmp_i][(int) tmp_j].value += (tmp_i + tmp_j);
+			map[(int) tmp_i][(int) tmp_j].houses++;
 		}
-		//sort (houseVec.begin(), houseVec.begin() + houseVec.size());
+		
 		count = 0;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				for (int k = 0; k < map[i][j].size(); k++) {
-					for(int inner_i = i; inner_i < 10; inner_i++) {
-						for (int inner_j = j; inner_j < 10; inner_j++) {
-							for (int inner_k = 0; inner_k < map[inner_i][inner_j].size(); inner_k++) {
-								sum += map[inner_i][inner_j][inner_k].first - map[i][j][k].first;
-								sum += map[inner_i][inner_j][inner_k].second - map[i][j][k].second;
-							}
-						}
+		for (int foo = 0; foo < houseVec.size(); foo++) {
+			pair<double, double> house = houseVec[foo];
+			for(int inner_i = (int) house.first+1; inner_i < 10; inner_i++) {
+				for (int inner_j = (int) house.second+1; inner_j < 10; inner_j++) {
+					if (map[inner_i][inner_j].houses > 0){
+						tmp = house.first + house.second;
+						sum += map[inner_i][inner_j].value - tmp;
+						count+= map[inner_i][inner_j].houses;
 					}
 				}
 			}
 		}
+		
 		sum /= (double) count;
 		printf("%.8f\n", sum);
 	}
